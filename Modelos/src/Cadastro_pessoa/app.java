81d61d31354a9a3c99ff4a2baf84d600580bfa7d
Arthur Ávila <arthur_ti@outlook.com>
2022-12-19 20:53:29 -0300

    as

--- Modelos/src/Cadastro_pessoa/app.java
+++ Modelos/src/Cadastro_pessoa/app.java
@@ -1,9 +1,12 @@
 package Cadastro_pessoa;
+
 public class app {
-    public static void main(String[] args) {
-		
+	public static void main(String[] args) {
+
 		pessoa n1 = new pessoa();
-		
+
 		n1.coleta();
-    
+
+	}
+
 }
