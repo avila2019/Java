80ce1e77a2358ddb0b103baaba0dddf02ab95056
Arthur Ávila <arthur_ti@outlook.com>
2023-01-19 19:36:46 -0300

    as

---
+++ Modelos/src/informacao.java
@@ -0,0 +1,10 @@
+public class informacao {
+    
+}
+
+// array list 
+
+// list.add("DADO") - Para adicionar dados no arraylist
+// list.remove("DADO") - Para remover dados do arraylist
+// lista.get(index do elemento)- Para pegar o dado no arraylist
+// lista.indexOf("DADO")- pesquisa elemento no arraylist
